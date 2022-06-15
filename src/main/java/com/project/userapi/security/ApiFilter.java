3a46be9ccd26fbacf52b90fb5d60851bf016bc7d
DonovanTarsis <donovan.engenharia@hotmail.com>
2022-06-15 18:16:47 -0300

    implementação do filtro de segurança

---
+++ src/main/java/com/project/userapi/security/ApiFilter.java
@@ -0,0 +1,32 @@
+package com.project.userapi.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.project.userapi.service.RestService;
+
+public class ApiFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        if(request.getHeader("Authorization") != null){
+
+            Authentication auth = RestService.validate(request);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
